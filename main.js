424d3b09ca28a8bcb1cb3ca0ae8bb3f687112c0c
frostbite69425 <medhihirak3@gmail.com>
2026-06-04 09:36:51 +0530

    add: driver script

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 import Tree from "./TreeClass.js";
 
-let test = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
-
 const prettyPrint = (node, prefix = "", isLeft = true) => {
   if (node === null || node === undefined) {
     return;
@@ -16,48 +14,52 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
   prettyPrint(node.leftChildren, `${prefix}${isLeft ? "    " : "│   "}`, true);
 };
 
-// prettyPrint(test.root);
-test.insert(70);
-test.insert(71);
-test.insert(72);
-test.insert(8.5);
-// console.log(`\n`);
-prettyPrint(test.root);
-// console.log(`\n`);
-// test.levelOrderForEachIterative((el) => console.log(el));
-// console.log(`\n`);
-// test.levelOrderForEachRecursive((el) => console.log(el));
-// console.log(`\n`);
-// test.inOrderForEach((el) => console.log(el));
-// console.log(`\n`);
-// test.preOrderForEach((el) => console.log(el));
-// console.log(`\n`);
-// test.postOrderForEach((el) => console.log(el));
+const createRandomArr = (n = 10) => {
+  const returnArr = [];
+  for (let i = 0; i < n; i++) {
+    let randomNum = Math.floor(Math.random() * 100);
+    returnArr.push(randomNum);
+  }
+  return returnArr;
+};
 
-console.log(`\n`);
-console.log(`height: ${test.height(4)}`);
-console.log(`\n`);
-console.log(`depth: ${test.depth(4)}`);
-console.log(`\n`);
-console.log(test.isBalanced());
-console.log(test.rebalance());
-console.log(`\n`);
-prettyPrint(test.root);
-console.log(`\n`);
-console.log(test.isBalanced());
-console.log(test.includes(97460743));
-// let newTree = test.rebalance();
-// console.log(`\n`);
-// prettyPrint(newTree.root);
-// console.log(newTree.isBalanced());
+const driver = () => {
+  const arr = createRandomArr();
+  const tree = new Tree(arr);
+  prettyPrint(tree.root);
+  console.log(`\n`);
+  console.log(tree.isBalanced());
+  console.log(`\n`);
+  console.log("preorder");
+  tree.preOrderForEach((val) => console.log(val));
+  console.log(`\n`);
+  console.log("inorder");
+  tree.inOrderForEach((val) => console.log(val));
+  console.log(`\n`);
+  console.log("postorder");
+  tree.postOrderForEach((val) => console.log(val));
+  console.log(`\n`);
+  tree.insert(101);
+  tree.insert(103);
+  tree.insert(145);
+  tree.insert(190);
+  prettyPrint(tree.root);
+  console.log(`\n`);
+  console.log(tree.isBalanced());
+  tree.rebalance();
+  console.log(`\n`);
+  prettyPrint(tree.root);
+  console.log(`\n`);
+  console.log(tree.isBalanced());
+  console.log(`\n`);
+  console.log("preorder");
+  tree.preOrderForEach((val) => console.log(val));
+  console.log(`\n`);
+  console.log("inorder");
+  tree.inOrderForEach((val) => console.log(val));
+  console.log(`\n`);
+  console.log("postorder");
+  tree.postOrderForEach((val) => console.log(val));
+};
 
-// test.deleteItem(7);
-// prettyPrint(test.root);
-// console.log(`\n`);
-// test.deleteItem(1);
-// prettyPrint(test.root);
-// console.log(`\n`);
-// test.deleteItem(67);
-// prettyPrint(test.root);
-// console.log(test.includes(69));
-// console.log(test.includes(67));
+driver();
